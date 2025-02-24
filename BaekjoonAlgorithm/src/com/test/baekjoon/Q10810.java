55e6aa6d802509b636815396a387b78cd464fb4f
NaraeP <skfocjstk93@naver.com>
2025-02-24 23:58:25 +0900

    Add. 24FEB25 baekjoon Q27866, Update. Q10810

--- BaekjoonAlgorithm/src/com/test/baekjoon/Q10810.java
+++ BaekjoonAlgorithm/src/com/test/baekjoon/Q10810.java
@@ -7,22 +7,39 @@ import java.util.StringTokenizer;
 
 public class Q10810 {
 	public static void main(String[] args) throws IOException {
+		/*
+		 * 주의! array의 idx가 0이 아닌 1부터("1번 바구니") 시작
+		 * 바구니 >> 배열 의미
+		 */
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		StringBuilder sb = new StringBuilder();
 		
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken()); // arr.length
+		int m = Integer.parseInt(st.nextToken()); // 대입 횟수
+		int[] arr = new int[n];
 		
-		for (int idx=0; idx<m; idx++) {
+		for (int x=0; x<m; x++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			
-			int i = Integer.parseInt(st.nextToken());
-			int j = Integer.parseInt(st.nextToken());
-			int k = Integer.parseInt(st.nextToken());
+			int i = Integer.parseInt(st.nextToken()); // arr[i] ~
+			int j = Integer.parseInt(st.nextToken()); // ~ arr[j]
+			int k = Integer.parseInt(st.nextToken()); // arr[i] = k;
 			
-			
-			
+			for (;i<=j; i++) { // 1번 바구니 부터
+				arr[i-1] = k;
+//				System.out.println("arr[" + (i-1) + "]: " + arr[i-1]);
+			}
 		}
+		
+		for (int i=0; i<arr.length; i++) {
+			sb.append(arr[i]).append(" ");
+		}
+		
+		sb.deleteCharAt(sb.length()-1);
+		
+		System.out.println(sb.toString());
 		
 	}
 }
